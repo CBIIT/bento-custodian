c59490e8780104a47be23713c68e2070a8ae9da5
Vincent Donkor <vincent.donkor@gmail.com>
2020-08-12 10:26:00 -0400

    changed credential to profile

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-//  profile = var.profile
+  profile = var.profile
   region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  //access_key = var.access_key
+  //secret_key = var.secret_key
 }
