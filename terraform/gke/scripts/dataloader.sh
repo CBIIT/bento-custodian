8774e274519d069a5b294ea48e09deaf66fd37d7
Vincent Donkor <vincent.donkor@gmail.com>
2020-08-18 22:38:38 -0400

    added gke to ansible

---
+++ terraform/gke/scripts/dataloader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+cd /tmp
+rm -rf bento-custodian || true
+git clone https://github.com/CBIIT/bento-custodian
+cd bento-custodian/ansible
+ansible-playbook data-loader.yml -e neo4j_ip=${neo4j_ip} -e neo4j_password=${neo4j_password}
