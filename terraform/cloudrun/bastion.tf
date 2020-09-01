b394ed0eae2ed2cbf604c3f01e8d4ec70b27ca8c
Vincent Donkor <vincent.donkor@gmail.com>
2020-09-01 10:17:45 -0400

    update cloudrun

--- terraform/cloudrun/bastion.tf
+++ terraform/cloudrun/bastion.tf
@@ -31,7 +31,7 @@ resource "google_compute_instance" "bastion" {
     connection {
       type = "ssh"
       user = var.ssh_user
-      private_key = file("${path.module}/ansible/ssh_private_key.pem")
+      private_key = local.private_key
       agent = "false"
       host = self.network_interface.0.access_config.0.nat_ip
     }
