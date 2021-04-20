e4c743869f731a9da4bc25f94975aef9bf507f67
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-04-20 14:50:42 -0400

    changed workflow by removing update stage
    Former-commit-id: 8810a337e126a992b6c519aee2ca3e4966762594

--- terraform/cloudrun/password.sh
+++ terraform/cloudrun/password.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 eval "$('@sh "export NEO4J_PASSWORD=\(.neo4j_password)"')"
-bearer=`echo "Basic $(echo -n "neo4j:$NEO4J_PASSWORD" | base64)"`
-printf '{"bearer": "%s"}\n' "$bearer" 
+BEARER=`echo "Basic $(echo -n "neo4j:$NEO4J_PASSWORD" | base64)"`
+printf '{"bearer": "%s"}\n' "$BEARER"
