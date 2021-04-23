e82b2e3e44d86ad8bf99717226d2c427ccc6958c
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-04-23 00:05:48 -0400

    modified dependency
    Former-commit-id: 336faef7ca7ccfb243a701eb0ac5177dc87691ca

--- terraform/cloudrun/bastion.tf
+++ terraform/cloudrun/bastion.tf
@@ -36,34 +36,34 @@ resource "google_compute_instance" "bastion" {
       host = self.network_interface.0.access_config.0.nat_ip
     }
   }
-  provisioner "file" {
-    source = "${path.module}/bastion.sh"
-    destination = "/tmp/bastion.sh"
-
-    connection {
-      type = "ssh"
-      user = var.ssh_user
-//      private_key = file("${path.module}/ansible/ssh_private_key.pem")
-      private_key = local.private_key
-      agent = "false"
-      host = self.network_interface.0.access_config.0.nat_ip
-    }
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/bastion.sh",
-      "sudo /tmp/bastion.sh",
-    ]
-    connection {
-      type = "ssh"
-      user = var.ssh_user
-      private_key = local.private_key
-      agent = "false"
-      host = google_compute_instance.bastion.network_interface.0.access_config.0.nat_ip
-    }
-  }
+//  provisioner "file" {
+//    source = "${path.module}/bastion.sh"
+//    destination = "/tmp/bastion.sh"
+//
+//    connection {
+//      type = "ssh"
+//      user = var.ssh_user
+////      private_key = file("${path.module}/ansible/ssh_private_key.pem")
+//      private_key = local.private_key
+//      agent = "false"
+//      host = self.network_interface.0.access_config.0.nat_ip
+//    }
+//  }
+//  provisioner "remote-exec" {
+//    inline = [
+//      "chmod +x /tmp/bastion.sh",
+//      "sudo /tmp/bastion.sh",
+//    ]
+//    connection {
+//      type = "ssh"
+//      user = var.ssh_user
+//      private_key = local.private_key
+//      agent = "false"
+//      host = google_compute_instance.bastion.network_interface.0.access_config.0.nat_ip
+//    }
+//  }
   tags = ["bastion"]
-  depends_on = [local_file.init_script]
+//  depends_on = [local_file.init_script]
 }
 
 resource "local_file" "update" {
@@ -93,7 +93,35 @@ resource "local_file" "db_loader" {
   filename = "${path.module}/loader.sh"
 }
 
+resource "null_resource" "build_image" {
+  provisioner "file" {
+    source = "${path.module}/bastion.sh"
+    destination = "/tmp/bastion.sh"
 
+    connection {
+      type = "ssh"
+      user = var.ssh_user
+      //      private_key = file("${path.module}/ansible/ssh_private_key.pem")
+      private_key = local.private_key
+      agent = "false"
+      host = self.network_interface.0.access_config.0.nat_ip
+    }
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bastion.sh",
+      "sudo /tmp/bastion.sh",
+    ]
+    connection {
+      type = "ssh"
+      user = var.ssh_user
+      private_key = local.private_key
+      agent = "false"
+      host = google_compute_instance.bastion.network_interface.0.access_config.0.nat_ip
+    }
+  }
+  depends_on = [local_file.init_script]
+}
 
 resource "null_resource" "update_deployment" {
 
